f6c676db9d12345cb07e4b71923537ec2c8cf538
Alexis Gutierrez <alexisgutierrezf.1997@gmail.com>
2020-02-05 23:51:20 -0500

    Using spring security for user login ms

---
+++ ms-user-management/src/main/java/com/dev/app/msusermanagement/config/WebSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.dev.app.msusermanagement.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	@Qualifier("userDetailsService")
+	private UserDetailsService userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and()
+		//starts authorizing configuration
+		.authorizeRequests()
+		.antMatchers("/resources/**", "/error", "/service/**").permitAll()
+		.anyRequest().fullyAuthenticated()
+		.and()
+		.logout().permitAll()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/service/logout", "POST"))
+		//login form
+		.and()
+			.formLogin().loginPage("/service/login").and()
+			//enable basic header authentication
+			.httpBasic()
+			.and()
+			
+			.csrf().disable();
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
+}
