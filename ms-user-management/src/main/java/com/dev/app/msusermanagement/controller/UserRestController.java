a3dce1030de25ec352821b8f106d9803e76cb435
Alexis Gutierrez <alexisgutierrezf.1997@gmail.com>
2020-02-06 23:24:12 -0500

    User service enable eureka discovery client

--- ms-user-management/src/main/java/com/dev/app/msusermanagement/controller/UserRestController.java
+++ ms-user-management/src/main/java/com/dev/app/msusermanagement/controller/UserRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,30 @@ public class UserRestController {
 	@Autowired
 	@Qualifier("userService")
 	private UserService userService;
+	
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private Environment env;
+	
+	@Value("${spring.application.name}")
+	private String serviceId;
+	
+	@GetMapping("/service/port")
+	String getPort() {
+		return "Service port number: " + env.getProperty("server.port");
+	}
+	
+	@GetMapping("/service/instance")
+	ResponseEntity<?> getInstances() {
+		return new ResponseEntity<>(discoveryClient.getInstances(serviceId), HttpStatus.OK);
+	}
+	
+	@GetMapping("/service/services")
+	ResponseEntity<?> getServices() {
+		return new ResponseEntity<>(discoveryClient.getServices(), HttpStatus.OK);
+	}
 	
 	@PostMapping("/service/registration")
 	ResponseEntity<?> saveUser(@RequestBody User user) {
